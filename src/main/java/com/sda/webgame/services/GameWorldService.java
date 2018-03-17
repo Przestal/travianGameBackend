7ae9965b8da21c58d8ae5a28397f6ec3fcee996a
przestal <przemyslawg91@gmail.com>
2018-03-17 15:59:03 +0100

    added log4j

--- src/main/java/com/sda/webgame/services/GameWorldService.java
+++ src/main/java/com/sda/webgame/services/GameWorldService.java
@@ -8,6 +8,7 @@ import com.sda.webgame.model.factory.GameWorldFieldFactory;
 import com.sda.webgame.repositories.GameWorldFieldRepository;
 import com.sda.webgame.repositories.GameWorldRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,11 +24,15 @@ public class GameWorldService implements IGameWorldService {
 
     @Override
     public Optional<GameWorld> createGameWorld(GameWorldDto dto) {
-        GameWorld world = gameWorldRepository.save(GameWorldFactory.createGameWorld(dto));
-        if (world != null){
-            List<GameWorldField> fields = GameWorldFieldFactory.createFieldsForWorld(world);
-            gameWorldFieldRepository.save(fields);
-        }
+        GameWorld world = null;
+        try {
+            world = gameWorldRepository.save(GameWorldFactory.createGameWorld(dto));
+            if (world != null) {
+                List<GameWorldField> fields = GameWorldFieldFactory.createFieldsForWorld(world);
+                gameWorldFieldRepository.save(fields);
+            }
+
+        } catch (DataIntegrityViolationException exception){};
         return Optional.ofNullable(world);
     }
 
